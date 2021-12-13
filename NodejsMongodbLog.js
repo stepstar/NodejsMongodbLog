821298a49170c3844c62352ae9eacfc70ed44e7b
shibuxing <shibuxing@zufe.edu.cn>
2021-12-13 09:25:25 +0800

    database connected successfully

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910123')
 const app = express();
-app.use('/',express.static('html'));
-app.get("/input",(req,res)=>{
-    res.send(req.query);
-    console.log(req.query);
-});
-app.listen(60123);
-/**
- * const schema={
+
+const schema={
     name: String,
     age: Number,
     health: String,
     hobby: String
 }
 const Cat = mongoose.model('cat1', schema); 
-const kitty = new Cat({ name: 'test' });
-kitty.save().then(() => console.log('testmeow'));
- */
+
+app.use('/',express.static('html'));
+app.get("/input",(req,res)=>{
+    res.send(req.query);
+    console.log(req.query);
+    const kitty = new Cat({ name: req.query.first, health:req.query.second });
+    kitty.save().then(() => console.log('success'));
+});
+app.listen(60123);
